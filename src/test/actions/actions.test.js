e453cec266b8f04fd8fd1cfa8819da98b520fe43
tzvaita <tzvaita@gmail.com>
2020-11-23 17:00:23 +0200

    Add tests for single pokemon query

--- src/test/actions/actions.test.js
+++ src/test/actions/actions.test.js
@@ -2,7 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
-import { fetchData, fetchSingleData} from '../../actions';
+import { fetchData, fetchSingleData } from '../../actions';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
@@ -15,13 +15,13 @@ describe('async actions', () => {
   });
 });
 
-it('should get pokemon information', () => {
+it('should get all pokemon information', () => {
   fetchMock.onGet('/').reply(200, {
     data: [
-            {name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/"},
-            {name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/"},
-            {name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3"},
-          ]
+      { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+      { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+      { name: 'venusaur', url: 'https://pokeapi.co/api/v2/pokemon/3' },
+    ],
   });
 
   store.dispatch(fetchData()).then(() => {
@@ -29,13 +29,50 @@ it('should get pokemon information', () => {
       type: 'FETCH_DATA_SUCCESSFUL',
       payload: {
         data: [
-          {name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/"},
-          {name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/"},
-          {name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3"},
-        ]
+          { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+          { name: 'ivysaur', url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+          { name: 'venusaur', url: 'https://pokeapi.co/api/v2/pokemon/3' },
+        ],
       },
     }];
 
     expect(store.getActions()).toEqual(expectedActions);
+  }).catch(error => error);
+});
+
+it('Should get a single pokemon', () => {
+  fetchMock.onGet('/pokemon/pokemon').reply(200, {
+    data: {
+      name: 'bulbasaur',
+      stats: [
+        { base_stat: 45, effort: 0 },
+        { base_stat: 49, effort: 0 },
+        { base_stat: 49, effort: 0 },
+        { base_stat: 65, effort: 1 },
+        { base_stat: 65, effort: 0 },
+        { base_stat: 45, effort: 0 },
+      ],
+    },
+  });
+
+  store.dispatch(fetchSingleData('bulbasaur')).then(() => {
+    const expectedActions = [{
+      type: 'FETCH_SINGLE_DATA_SUCCESSFUL',
+      payload: {
+        data: {
+          name: 'bulbasaur',
+          stats: [
+            { base_stat: 45, effort: 0 },
+            { base_stat: 49, effort: 0 },
+            { base_stat: 49, effort: 0 },
+            { base_stat: 65, effort: 1 },
+            { base_stat: 65, effort: 0 },
+            { base_stat: 45, effort: 0 },
+          ],
+        },
+      },
+    }];
+
+    expect(store.getActions().toEqual(expectedActions));
   }).catch(error => error);
 });
