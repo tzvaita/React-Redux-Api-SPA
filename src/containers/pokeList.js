31fa0c165a3a103ccf67910b0f253bcecbae2826
tzvaita <tzvaita@gmail.com>
2020-12-08 22:18:46 +0200

    Add filter checking

--- src/containers/pokeList.js
+++ src/containers/pokeList.js
@@ -3,9 +3,14 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
-import { fetchData } from '../actions';
+import { fetchData, categoryFilter } from '../actions';
+import NavBar from '../components/navbar';
 
-const PokeList = ({ userData, fetchData }) => {
+const PokeList = ({
+  userData, fetchData, categoryFilter, filter,
+}) => {
+  const handleFilterChange = e => categoryFilter(e.target.value);
+  let list;
   useEffect(() => {
     fetchData();
   }, []);
@@ -14,24 +19,40 @@ const PokeList = ({ userData, fetchData }) => {
   }
 
   if (!_.isEmpty(userData.ourData)) {
+    (filter === 'ALL') ? (list = userData.ourData.map(pok => (
+      <div key={userData.ourData.indexOf(pok)} className="singleImg card col-lg-3 col-md-4 col-sm-6">
+        <Link to={`/pokemon/${pok.name}`}>
+          <img className="card-img-top" src={`https://pokeres.bastionbot.org/images/pokemon/${pok.id}.png`} alt="POKEMON" />
+          <div className="card-body">
+            <h5 className="card-title pokTitle">{pok.name}</h5>
+          </div>
+        </Link>
+        <div className="types">
+          <h5 className="pokTitle">Type</h5>
+          {pok.types.map(type => (
+            <div className="typetxt" key={pok.types.indexOf(type)}>{type.type.name}</div>))}
+        </div>
+      </div>
+    ))) : (list = userData.ourData.filter(pok => pok.category === filter).map(pok => pok => (
+      <div key={userData.ourData.indexOf(pok)} className="singleImg card col-lg-3 col-md-4 col-sm-6">
+        <Link to={`/pokemon/${pok.name}`}>
+          <img className="card-img-top" src={`https://pokeres.bastionbot.org/images/pokemon/${pok.id}.png`} alt="POKEMON" />
+          <div className="card-body">
+            <h5 className="card-title pokTitle">{pok.name}</h5>
+          </div>
+        </Link>
+        <div className="types">
+          <h5 className="pokTitle">Type</h5>
+          {pok.types.map(type => (
+            <div className="typetxt" key={pok.types.indexOf(type)}>{type.type.name}</div>))}
+        </div>
+      </div>)));
+
     return (
       <div className="container-fluid">
+        <NavBar handleFilterChange={handleFilterChange} />
         <div className="row">
-          {userData.ourData.map(pok => (
-            <div key={userData.ourData.indexOf(pok)} className="singleImg card col-lg-3 col-md-4 col-sm-6">
-              <Link to={`/pokemon/${pok.name}`}>
-                <img className="card-img-top" src={`https://pokeres.bastionbot.org/images/pokemon/${pok.id}.png`} alt="POKEMON" />
-                <div className="card-body">
-                  <h5 className="card-title pokTitle">{pok.name}</h5>
-                </div>
-              </Link>
-              <div className="types">
-                <h5 className="pokTitle">Type</h5>
-                {pok.types.map(type => (
-                  <div className="typetxt" key={pok.types.indexOf(type)}>{type.type.name}</div>))}
-              </div>
-            </div>
-          ))}
+          {list}
         </div>
       </div>
     );
@@ -51,13 +72,17 @@ PokeList.propTypes = {
     error: PropTypes.string,
   }).isRequired,
   fetchData: PropTypes.func.isRequired,
+  categoryFilter: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = state => ({
   userData: state.pokemon,
+  filter: state.filter,
 });
 
 const mapDispatchToProps = dispatch => ({
   fetchData: () => dispatch(fetchData()),
+  categoryFilter: category => dispatch(categoryFilter(category)),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(PokeList);
